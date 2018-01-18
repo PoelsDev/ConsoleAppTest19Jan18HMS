9fd3118b193005c09c06efa945c1770d4d2d73f4
Thibault Poels <thibault.poels@immalle.be>
2018-01-18 17:55:13 +0100

    Add project files.

---
+++ ConsoleAppTest19Jan18HMS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest19Jan18HMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
